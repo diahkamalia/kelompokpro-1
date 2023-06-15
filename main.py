b0654825760a8945dfcf63cc76e2be191feb6bda
LaylaMufahChoiriyah <101154990+LaylaMufahChoiriyah@users.noreply.github.com>
2023-06-15 08:25:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ with st.container():
 
     if choose == "Home":
         
-        st.markdown("Proyek Sains Data B")
+        st.markdown("# Proyek Sains Data B")
 
-        st.info( " # Nama Kelompok : ")
+        st.info( " Nama Kelompok : ")
         st.write("Layla Mufah Choiriyah - 200411100052")
         st.write("Diah Kamalia - 200411100061")
        
